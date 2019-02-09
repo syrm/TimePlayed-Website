ecd027494e172525bfc62d505dc8014cffce6e62
xVaql <valentijnvanwinden@gmail.com>
2019-02-09 11:29:30 +0100

    Add favicon

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var favicon = require('serve-favicon');
 const app = express()
 const port = 80
-var schedules = require('./schedules');
+
+require('./schedules');
 
 var botClient = require("./middlewares/botClient")
 
@@ -12,9 +14,10 @@ app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(session({secret: "SFJSDF4cvn4DSAFSF456f654xcv132Fsxc34dsFxcsfd"}));
 
-
 console.log("Starting bot up...")
 botClient.startClient(function(client) {
+  app.use(favicon(__dirname + '/public/images/favicon.ico'));
+
   app.use(express.static(__dirname + '/public'))
   app.use(require('./controllers'))
   app.use('/', require('./middlewares/discord'));
